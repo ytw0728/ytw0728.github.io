e1e0cf33aeafac83d136dd728b561248c379cb4d
ytw0728 <ytw0728@gmail.com>
2026-06-17 03:24:48 +0900

    Squashed 'docs/slash/' changes from 5370a5d..dcc6c48
    dcc6c48 release: v1.2.1
    
    git-subtree-dir: docs/slash
    git-subtree-split: dcc6c48de9c982300b7a964e7c60222bbaa1e8b7

--- install.win.ps1
+++ install.win.ps1
@@ -1,5 +1,5 @@
 $arch = if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") { "amd64" } else { "arm64" }
-$url = "https://ytw0728.github.io/docs/slash/clis/v1.2.0/s-win-$arch.exe"
+$url = "https://ytw0728.github.io/docs/slash/clis/v1.2.1/s-win-$arch.exe"
 $dest = "$HOME\AppData\Local\Microsoft\WindowsApps\s.exe"
 
 Write-Host "Downloading my-cli for Windows ($arch)..."
